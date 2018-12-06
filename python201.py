47c12497aea1abe3e4ede112d31bb7f5ec6c9070
duongdidom <cmduong.vn90+github@gmail.com>
2018-12-06 15:31:24 +1300

    try to use dictionary instead of list. Unsure how to loop through values

--- python201.py
+++ python201.py
@@ -4,15 +4,16 @@ import os   # import os module to change working directory
 import glob     # import glob module to file list of filename that match certain criteria
 from datetime import datetime, date, timedelta   # import datetime module to convert string to date format
 import shutil   # import shutil module to copy file
+import sys
 
 # define start date and end date
 startD = "09/11/2018" 
 endD = "10/11/2018"
 
 # define parent directory where input, cons, output folders are stored. This assumes all three folders are under a parent folder, mainly for testing purpose. Might need to modify in real situation
-parent_dir = r"C:\Users\douglas.cao\Google Drive\Python\RiskCapital"    # insert r before a string so that python interprete string as raw. C:\Users\DDD\Downloads\Test for home
+parent_dir = r"C:\Users\douglas.cao\Documents\Python\RiskCapital"    # insert r before a string so that python interprete string as raw. C:\Users\DDD\Downloads\Test for home
 
-#1. Collect input files:
+#1. Collect source and new files:
 def find_current_files(yyyymmdd,hhmmss):    # variable = date and current execution time
     out_timestamp = yyyymmdd + "-" + hhmmss + "_"
 
@@ -23,12 +24,12 @@ def find_current_files(yyyymmdd,hhmmss):    # variable = date and current execut
 
     latest_position = max(glob.iglob("SPANPOS_" + yyyymmdd + "*.txt"), key = os.path.getmtime) # find the latest modified file with specified format
     
-    pa2File = parent_dir + r"\\out\\" + out_timestamp + latest_pa2    # define output path for latest pa2 file
+    pa2_pa2 = parent_dir + r"\\out\\" + out_timestamp + latest_pa2    # define output path for latest pa2 file
 
-    posistionFile = parent_dir + r"\\out\\" + out_timestamp + latest_position   # define output path for latest position file
-
-    shutil.copy(os.getcwd() + "\\" + latest_pa2, pa2File) # copy() function copies from source to destination
-    shutil.copy(os.getcwd() + "\\" + latest_position, posistionFile) # copy latest pa2 and position file in input folder to output folder
+    position_txt = parent_dir + r"\\out\\" + out_timestamp + latest_position   # define output path for latest position file
+    
+    shutil.copy(os.getcwd() + "\\" + latest_pa2, pa2_pa2) # copy() function copies from source to destination
+    shutil.copy(os.getcwd() + "\\" + latest_position, position_txt) # copy latest pa2 and position file in input folder to output folder
 
     ### 1.2. find constant files and copy them to output folder
     # path and filename for cons files
@@ -42,7 +43,7 @@ def find_current_files(yyyymmdd,hhmmss):    # variable = date and current execut
     rc_intermonth_csv =  parent_dir + r"\\out\\" + out_timestamp + r"rc_intermonth.csv"
     rc_scan_csv =  parent_dir + r"\\out\\" + out_timestamp + r"rc_scan.csv"
     house_csv = parent_dir + r"\\out\\" + out_timestamp + r"house.csv"
-
+    
     # copy from cons folder to output folder
     try:
         shutil.copy(cons_rc_intercomm_csv,rc_intercomm_csv)
@@ -67,20 +68,117 @@ def find_current_files(yyyymmdd,hhmmss):    # variable = date and current execut
     pbreq_csv =  out_timestamp + r"pbreq_" 
     final_csv = out_timestamp + r"final.csv"
 
-    return (pa2File, posistionFile, rc_intercomm_csv,rc_intermonth_csv, rc_scan_csv, house_csv, new_pa2, sum_position_txt, whatif_xml, spanit_txt, span_spn, pbreq_csv, final_csv)
+    return (pa2_pa2, position_txt, rc_intercomm_csv,rc_intermonth_csv, rc_scan_csv, house_csv, new_pa2, sum_position_txt, whatif_xml, spanit_txt, span_spn, pbreq_csv, final_csv)
+
+#2. read pa2 & store data into various lists
+### 2.1. read 
+def read_pa2(pa2File):
+    with open(pa2File,"r") as f: # open pa2 file in read mode and name this file as f
+        pa2_list = f.readlines() # read all lines in pa2File and store each line into list
+    return (pa2_list)
+
+### 2.2. retrieve and store
+def parse_pa2(pa2_list): # from pa2 list, which is from original pa2 file, break down by record type and store them into several dictionaries
+    ### 2.2.1. create bunch of dictionary to store data
+    price_dict = {"comm":[],"instype":[], "maturity":[],"dsp":[]}             # for future prices
+    price_list = []
+    price_param_dict = {"comm":[],"instype":[], "dspdl":[],"cvf":[],"strikedl":[],"curr":[]}       # calculation parameter
+    price_param_list = []
+    intermonth_dict = {}        # intermonth spread charge
+    intermonth_param_dict = {}  # intermonth parameter details
+    intercomm_dict = {}         # intercommodity spread charge
+    intercomm_param_dict = {}   # intercommodity parameter details
+    instrument_dict = {}        # instrument type & maturity
+    option_dict = {}            # option prices and delta
+    deltascale_dict = {}        # instrument type, maturity & delta scaling factor
+    currency_dict = {}          # currency    
+    
+    for line in pa2_list:
+        # price_dict = dictionary of futures prices [commodity, type, maturity, price] eg ['WMP','FUT',201801,31200]
+        if line.startswith("82") and (line[25:28] == "FUT" or line[25:28] == "PHY"): # if record type 82, string from 26-29 = FUT or PHY
+            price_dict["comm"].append(line[5:15].strip())   # strip() to remove any space character
+            price_dict["instype"].append(line[25:28].strip())
+            price_dict["maturity"].append(int(line[29:35].strip() or 0))    # if blank, integer() function will use 0 
+            price_dict["dsp"].append(float(line[110:117].strip() or 0))
+
+        # elif line.startswith("82") and (str(line[25:28]) == "FUT" or str(line[25:28]) == "PHY"):
+            price_list.append({
+            'comm':str(line[5:15]).strip(),
+            'instype':str(line[25:28]),
+            'maturity':int(line[29:35].strip() or 0), 
+            'dsp':float(line[110:117].strip() or 0)
+            })
+            
+
+        elif line.startswith("P"):  # if record type start with P
+            price_param_dict["comm"].append(line[5:15].strip())
+            price_param_dict["instype"].append(line[15:18].strip())
+            price_param_dict["dspdl"].append(int(line[33:36].strip() or 0))
+            price_param_dict["cvf"].append(float(line[41:55].strip() or 0)/10000000)
+            price_param_dict["strikedl"].append(int(line[36:39].strip() or 0))
+            price_param_dict["curr"].append(line[65:68].strip())
+
+        # elif line.startswith("P"):
+            price_param_list.append({
+            'comm':str(line[5:15]).strip(),
+            'instype':str(line[15:18]),
+            'dspdl':int(line[33:36].strip() or 0),
+            'cvf':float(line[41:55].strip() or 0)/10000000,
+            'strikedl':int(line[36:39].strip() or 0),
+            'curr':str(line[65:68])
+            })
+        
+    print (price_dict)
+    # print (price_list)
+    print (price_param_dict)
+    # print (price_param_list)
+
+    ### 2.2.2. add extra details in price_dict from price_param_dict
+    for price in price_list:
+        appended = False
+        for price_param in price_param_list:
+            if price['comm'] == price_param['comm'] and price['instype'] == price_param['instype']:
+                price.update({
+                'dspdl':price_param['dspdl'],
+                'cvf':price_param['cvf']
+                })
+                appended = True
+                break
+        if not appended:
+            logging.error("Error with original SPAN parameters: this instrument type from record 8 does not"
+                " have an equivalent record P: " + str(price['comm']) + str(price['instype']))
+            price.update({
+            'dspdl':"NA",
+            'cvf':"NA"
+            })
+
+    print (price_list)
+    for value1 in price_dict["comm"]:
+        appended = False
+        n=0
+        for value2 in price_param_dict["comm"]:
+            m=0
+            if value1 == value2 and price_dict["instype"][n] == price_param_dict["instype"][n]:
+                print ("PAUSE" + value1 + " " + price_dict["instype"][n] + price_param_dict["instype"][m])
+                print (str(n) + " " + str(m))
+                n =+ 1
+                m =+ 1
 
 ############################### MAIN ###############################
 # convert start date and end date to date format. strptime = string parse time = retrieve time string
 startD = datetime.strptime(startD, "%d/%m/%Y")    # 2nd parameter after comma = how original date was formatted
 endD = datetime.strptime(endD, "%d/%m/%Y")  # output would be in date format, not string format
-dates = [startD + timedelta(x) for x in range(0, (endD-startD).days)]   # for each x from 0 to number days between start date and end date, convert x from integer to date. Then plus that number to start date. Put that into a list
+dates = [startD + timedelta(x) for x in range(0, (endD-startD).days)]   # for each x from 0 to count number days between start date and end date, convert x from integer to date. Then plus that number to start date. Put that into a list
 
 hhmmss = datetime.now().strftime("%H%M%S")  # define execution time to create time stamp in output files. strftime = string format time = format time string
 for date in dates:  # loop for all date in dates list
     #1. Collect input files (cons, input). Define newly created file and path. 
-    find_current_files(date.strftime("%Y%m%d"), hhmmss)
-    #2. read pa2 file
-
+    (pa2File, posistionFile, rc_intercomm_csv,rc_intermonth_csv, rc_scan_csv, house_csv, new_pa2, sum_position_txt, whatif_xml, spanit_txt, span_spn, pbreq_csv, final_csv) = find_current_files(date.strftime("%Y%m%d"), hhmmss)
+    
+    #2. read pa2 file & store data into various lists
+    pa2_list = read_pa2(pa2File)
+    parse_pa2(pa2_list)
+    
     #3. read 3 constant files
 
     #4. calculate new stretched scan range, intermonth, intercomm
