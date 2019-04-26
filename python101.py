5aa1bb3683c63945b1a8327eaab57461dfe20d3f
duongdidom1 <cmduong.vn90+git@gmail.com>
2019-04-27 11:39:05 +1200

    test new folder

--- python101.py
+++ python101.py
@@ -59,11 +59,11 @@
 # print (latest_position_filepath)
 
 
-# lesson 5: anconda with VS code: https://www.youtube.com/watch?v=g-aS9oVY-DA
-import matplotlib.pyplot as plt
-import numpy as np
+# # lesson 5: anconda with VS code: https://www.youtube.com/watch?v=g-aS9oVY-DA
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+# x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+# plt.plot(x, np.sin(x))       # Plot the sine of each x point
+# plt.show()                   # Display the plot
 
